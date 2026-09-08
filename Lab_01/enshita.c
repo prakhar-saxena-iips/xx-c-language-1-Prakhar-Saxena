7dbb1186c1ae646cf326d5a2ef0e96dde8631380
prakhar-saxena-iips <prakhariips21@gmail.com>
2026-09-08 12:08:22 +0530

    Add files via upload

---
+++ Lab_01/enshita.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//#include<conio.h>
+int main() {
+int age;
+clrscr();
+printf("enter your age\n:");
+scanf("%d\n" ,&age);
+printf("you entered: %d\n" ,age);
+getch()
+return 0;
+}
