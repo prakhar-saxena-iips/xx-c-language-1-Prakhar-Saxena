3397a044a2391c6f8788f2d1c8b5923b6cb10584
prakhar-saxena-iips <prakhariips21@gmail.com>
2026-09-12 01:33:43 +0530

    Implement pre and post decrement examples in C
    Added examples of pre and post decrement operations in C.

--- Lab-04/Decrement.c
+++ Lab-04/Decrement.c
@@ -1,1 +1,33 @@
+Pre decrement 
+#include <stdio.h>
 
+int main() {
+    int a=69;
+    int b=--a;
+    printf("By Prakhar Saxena\n");
+    printf("%d%d",a,b);
+
+    return 0;
+}
+Output 
+By Prakhar Saxena
+6868
+
+=== Code Execution Successful ===
+
+Post decrement 
+#include <stdio.h>
+
+int main() {
+    int a=69;
+    int b=a--;
+    printf("By Prakhar Saxena\n");
+    printf("%d%d",a,b);
+
+    return 0;
+}
+Output
+By Prakhar Saxena
+6869
+
+=== Code Execution Successful ===
