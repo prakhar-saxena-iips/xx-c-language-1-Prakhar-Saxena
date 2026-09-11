3f487cf2d16561a3d86b38fcad6ce56cc3e14bac
prakhar-saxena-iips <prakhariips21@gmail.com>
2026-09-12 01:30:19 +0530

    Implement pre-increment and post-increment examples
    Added examples of pre-increment and post-increment operations in C.

--- Lab-04/Increment.c
+++ Lab-04/Increment.c
@@ -1,1 +1,30 @@
+Pre increment 
+#include <stdio.h>
 
+int main() {
+    int a=5;
+    int b=++a;
+    printf("%d%d",a,b);
+
+    return 0;
+}
+Output 
+66
+
+=== Code Execution Successful ===
+
+
+Post increment 
+#include <stdio.h>
+
+int main() {
+    int a=69;
+    int b=a++;
+    printf("%d%d",a,b);
+
+    return 0;
+}
+Output 
+7069
+
+=== Code Execution Successful ===
