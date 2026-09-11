5e8bd43fc9e590df75f5ecd0de31a9cd93a6d2d0
prakhar-saxena-iips <prakhariips21@gmail.com>
2026-09-11 12:10:48 +0530

    Add division program in C
    This program takes two floating-point numbers as input and outputs their division.

---
+++ lab-02/division.c
@@ -0,0 +1,27 @@
+DIVISION 
+
+#include <stdio.h>
+
+int main() 
+    {
+    float a;
+    float b;
+    printf("By Prakhar Saxena\n");
+    printf("Enter number A-\n");
+    scanf("%f",&a);
+    printf("Enter number B-\n");
+    scanf("%f",&b);
+    printf("Your division is %f\n",a/b);
+    return 0;
+    }
+
+Output
+By Prakhar Saxena
+Enter number A-
+138
+Enter number B-
+2
+Your division is 69.000000
+
+
+=== Code Execution Successful ===
