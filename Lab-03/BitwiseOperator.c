69e395261234075b26f453a2d09ce42cafaf86c9
prakhar-saxena-iips <prakhariips21@gmail.com>
2026-09-11 12:30:30 +0530

    Update BitwiseOperator.c

--- Lab-03/BitwiseOperator.c
+++ Lab-03/BitwiseOperator.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    int a=5;
+    int b=25;
+    printf("By Prakhar Saxena\n");
+    printf("%d\n",a&b);
+    printf("%d\n",a|b);
+    printf("%d\n",a^b);
+    printf("%d\n",~a);
+    printf("%d\n",~b);
+    printf("%d\n",a<<b);
+    printf("%d\n",a>>b);
+
+    return 0;
+}
+
+
+
+Output - By Prakhar Saxena
+1
+29
+28
+-6
+-26
+167772160
+0
+
+
+=== Code Execution Successful ===
