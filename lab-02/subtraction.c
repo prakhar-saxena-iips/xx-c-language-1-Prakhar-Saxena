e0085b54075fbf3b9b0435068043e4b2f23eddbe
prakhar-saxena-iips <prakhariips21@gmail.com>
2026-09-11 12:08:14 +0530

    Create subtraction.c

---
+++ lab-02/subtraction.c
@@ -0,0 +1,27 @@
+Subtraction 
+
+#include <stdio.h>
+
+int main() 
+    {
+    int a;
+    int b;
+    printf("By Prakhar Saxena\n");
+    printf("Enter number A-\n");
+    scanf("%d",&a);
+    printf("Enter number B-\n");
+    scanf("%d",&b);
+    printf("Your subtraction is %d",a-b);
+    return 0;
+    }
+
+Output 
+By Prakhar Saxena
+Enter number A-
+79
+Enter number B-
+10
+Your subtraction is 69
+
+=== Code Execution Successful ===
+
