76172f8123f13b6bd5e9da5ab2cbbbf75270aa6a
prakhar-saxena-iips <prakhariips21@gmail.com>
2026-09-12 01:23:18 +0530

    Implement integer equality check in RelationalOperator.c
    Added a program to compare two integers and check if they are equal.

--- Lab-03/RelationalOperator.c
+++ Lab-03/RelationalOperator.c
@@ -1,1 +1,44 @@
 
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    printf("By Prakhar Saxena\n");
+    printf("Enter number A-\n");
+    scanf("%d",&a);
+    printf("Enter number B-\n");
+    scanf("%d",&b);
+
+    if (a==b)
+    {
+        printf("Your numbers are equal.\n");
+    }
+
+    else {
+    printf("Your numbers are not equal.\n");
+    }
+    return 0;
+}
+
+Output 1
+By Prakhar Saxena
+Enter number A-
+76
+Enter number B-
+78
+Your numbers are not equal.
+
+
+=== Code Execution Successful ===
+
+Output 2
+By Prakhar Saxena
+Enter number A-
+69
+Enter number B-
+69
+Your numbers are equal.
+
+
+=== Code Execution Successful ===
