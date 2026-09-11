28c806de621e37766e66fd22585c0e39a759242d
prakhar-saxena-iips <prakhariips21@gmail.com>
2026-09-12 01:35:51 +0530

    Implement example of Pre and Post increment in C
    Added print statements to display author name.

--- Lab-04/Increment.c
+++ Lab-04/Increment.c
@@ -4,11 +4,13 @@ Pre increment
 int main() {
     int a=5;
     int b=++a;
+    printf("By Prakhar Saxena\n");
     printf("%d%d",a,b);
 
     return 0;
 }
 Output 
+By Prakhar Saxena
 66
 
 === Code Execution Successful ===
@@ -20,6 +22,7 @@ Post increment
 int main() {
     int a=69;
     int b=a++;
+    printf("By Prakhar Saxena\n");
     printf("%d%d",a,b);
 
     return 0;
